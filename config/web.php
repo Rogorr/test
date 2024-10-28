5083765b16055ca7da1f0a8c20da02f9c1b731a8
Rogor <var.dan02@mail.ru>
2024-10-28 16:38:10 +0300

    lesson_5

--- config/web.php
+++ config/web.php
@@ -16,6 +16,9 @@ $config = [
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
+        'formatter' => [
+            'dateFormat' => 'php: d F Y',
+       ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Iia2fHK6kJnNDY-ELfGj0AKO-EGcuMO9',
