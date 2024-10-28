36904286ec047948d4ca12be7157bda76a7407a1
Rogor <var.dan02@mail.ru>
2024-10-28 17:58:15 +0300

    lesson_7

--- config/web.php
+++ config/web.php
@@ -22,6 +22,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Iia2fHK6kJnNDY-ELfGj0AKO-EGcuMO9',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -49,14 +50,17 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'page/<page:\d+>' => 'post/index',
+                '/' => 'post/index',
+                'post/<id:\d+>' =>'post/view',
+                'category/<alias:[a-zA-Z0-9-]+>' =>'category/view',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
